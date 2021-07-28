2558a3003f10c30c29deaa6ffbeac9fa89d00d48
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-07-28 21:34:49 +0700

    Create cards of passengers

---
+++ src/views/PassengerList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="list">
+    <PassengerCard
+      v-for="passenger in passengers"
+      :key="passenger.id"
+      :passenger="passenger"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PassengerCard from "@/components/PassengerCard.vue";
+import EventService from "@/services/EventService.js";
+
+export default {
+  name: "PassengerList",
+  components: {
+    PassengerCard,
+  },
+  data() {
+    return {
+      passengers: null
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.passengers = response.data.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+
+</script>
