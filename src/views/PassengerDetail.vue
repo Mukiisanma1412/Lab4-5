6572509144f7c722378130aea16b5561c2b2620a
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-07-29 16:54:16 +0700

    Change airline card to check if it's array

--- src/views/PassengerDetail.vue
+++ src/views/PassengerDetail.vue
@@ -2,14 +2,18 @@
   <div v-if="passenger">
     <h1>{{ passenger.name }}</h1>
     <p>Trips : {{ passenger.trips }}</p>
-   <p>Airline : {{ passenger.airline.name }} </p>
-<div class="list">  
-    <AirlineCard
-      v-for="airline in airlines"
-      :key="airline.id"
-      :airline="airline"
-    />
-  </div>
+
+    <div class="list">
+      <p v-if="!(Array.isArray(passenger.airline))">
+        Airline : {{ airlines.name }}
+      </p>
+      <AirlineCard
+        v-else
+        v-for="airline in airlines"
+        :key="airline.id"
+        :airline="airline"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,26 +21,26 @@
 // @ is an alias to /src
 
 import EventService from "@/services/EventService.js";
-import AirlineCard from "@/components/AirlineCard.vue"
+import AirlineCard from "@/components/AirlineCard.vue";
 
 export default {
   name: "PassengerDetail",
   props: ["id"],
-  components:{
+  components: {
     AirlineCard,
   },
 
   data() {
     return {
       passenger: null,
-      airlines: null
+      airlines: null,
     };
   },
   created() {
     EventService.getPassengerDetail(this.id)
       .then((response) => {
         this.passenger = response.data;
-        this.airlines = response.data.airline
+        this.airlines = response.data.airline;
       })
       .catch((error) => {
         console.log(error);
